03896bf14e1e96770952804fa5b8bb5636688797
Raghav Kattel <arg.ahv7@gmail.com>
2021-12-29 11:27:45 +0545

    ui changes

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,14 +7,14 @@ module.exports = {
     // You can overwrite values here that are used for the SEO component
     // You can also add new values here to query them like usual
     // See all options: https://github.com/LekoArts/gatsby-themes/blob/main/themes/gatsby-theme-cara/gatsby-config.js
-    siteTitle: `Cara`,
-    siteTitleAlt: `Cara - Gatsby Starter Portfolio`,
-    siteHeadline: `Cara - Gatsby Theme from @lekoarts`,
-    siteUrl: `https://cara.lekoarts.de`,
-    siteDescription: `Playful and Colorful One-Page portfolio featuring Parallax effects and animations`,
+    siteTitle: `Raghav`,
+    siteTitleAlt: `Raghav Kattel`,
+    siteHeadline: `Raghav Kattel`,
+    siteUrl: `https://raghavkattel.com`,
+    siteDescription: `A personal portfolio for Raghav`,
     siteLanguage: `en`,
     siteImage: `/banner.jpg`,
-    author: `@lekoarts_de`,
+    author: `@argahv`,
   },
   plugins: [
     {
@@ -25,9 +25,9 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Cara - @lekoarts/gatsby-theme-cara`,
-        short_name: `Cara`,
-        description: `Playful and Colorful One-Page portfolio featuring Parallax effects and animations`,
+        name: `Raghav`,
+        short_name: `argahv`,
+        description: `A personal portfolio for Raghav`,
         start_url: `/`,
         background_color: `#141821`,
         // This will impact how browsers show your PWA/website
